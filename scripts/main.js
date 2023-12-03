a75bfaab87d8981e7f2f9eeb289462ab8616e6fd
Dmytro Dychenko <DimaDichenko@gmail.com>
2023-12-03 23:59:23 +0100

    main.html, main.css, main.js - ready

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,12 @@ let lang = "en";
 
 let bikesList = null;
 let citiesList = null;
+let translations = null;
+
+let nameOk = false;
+let phoneOk = false;
+let emailOk = false;
+let commentOk = false;
 
 let application = {
     name: null,
@@ -16,46 +22,49 @@ let application = {
     timestamp: null
 };
 
-const translations = {
-    en: {
-        bikeListButton: "Go to Bike List",
-        applicationButton: "Go to Application",
-        noBikesMessage: 'Bicycles are currently not available',
-        applyButton: 'Send application',
-        rentalText: 'Rental'
-    },
-    pl: {
-        bikeListButton: "Przejdź do listy rowerów",
-        applicationButton: "Przejdź do wniosku",
-        noBikesMessage: 'Obecnie rowery nie są dostępne',
-        applyButton: 'Złożyć wniosek',
-        rentalText: 'Cena'
-    },
-    ua: {
-        bikeListButton: "Перейти до списку велосипедів",
-        applicationButton: "Перейти до заявки",
-        noBikesMessage:'Велосипеди зараз недоступні',
-        applyButton: 'Подати заявку',
-        rentalText: 'Цiна'
-    },
-    ru: {
-        bikeListButton: "Перейти к списку велосипедов",
-        applicationButton: "Перейти к заявлению",
-        noBikesMessage: 'Велосипеды в данный момент недоступны',
-        applyButton: 'Подать заявку',
-        rentalText: 'Цена'
-    }
-};
+fetch('./resources/translations.json')
+    .then(response => response.json())
+    .then(data => {
+        translations = data;
+    })
+    .catch(error => {
+        console.error('Error to getting data: ', error);
+    });
+
+function scrollToApplicationBlock() {
+    let applicationBlock = document.getElementById('send-application-block');
+    let blockPosition = applicationBlock.getBoundingClientRect().top + window.scrollY;
+
+    window.scrollTo({
+        top: blockPosition,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToBikesListBlock() {
+    let bikeListBlock = document.getElementById('bike-list-block');
+    let blockPosition = bikeListBlock.getBoundingClientRect().top + window.scrollY;
+
+    window.scrollTo({
+        top: blockPosition - 115,
+        behavior: 'smooth'
+    });
+}
+
+function setRandomBackgroundImage() {
+    let randomNumber = Math.floor(Math.random() * 7) + 1;
+    let imagePath = '/media/cover-image' + randomNumber + '.png';
+    document.body.style.backgroundImage = 'url("' + imagePath + '")';
+}
 
 document.getElementById('to-application-btn').addEventListener('click', function() {
-    // Scroll to the send-application-block
-    document.getElementById('send-application-block').scrollIntoView();
+    scrollToApplicationBlock();
 });
 
 document.getElementById('to-bike-list-btn').addEventListener('click', function() {
-    // Scroll to the bike-list-block
-    document.getElementById('bike-list-block').scrollIntoView();
+    scrollToBikesListBlock();
 });
+
 
 document.getElementById('language-dropdown').addEventListener('change', function() {
     lang = this.value;
@@ -68,6 +77,7 @@ document.getElementById('language-dropdown').addEventListener('change', function
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+    setRandomBackgroundImage();
     fetchCities();
     fetchBikes();
 });
@@ -96,10 +106,25 @@ function fetchCities() {
         });
 }
 
-function displayBikes() {
+function displayBikes(retryCount = 0) {
     if (!bikesList || !bikesList.length || !citiesList || !citiesList.length) {
-        displayNoBikes();
-        return;
+        if (retryCount < 3) {
+            setTimeout(() => displayBikes(retryCount + 1), 200);
+            return;
+        } else {
+            displayNoBikes();
+            return;
+        }
+    }
+
+    if(!translations) {
+        if (retryCount < 3) {
+            setTimeout(() => displayBikes(retryCount + 1), 200);
+            return;
+        } else {
+            displayNoBikes();
+            return;
+        }
     }
 
     bikeListBlock.innerHTML = bikesList.map(bike => {
@@ -115,9 +140,12 @@ function displayBikes() {
                     <button id="next" onclick="changePhoto(${bike.id}, 'next')">→</button>
                 </div>
                 <div class="bike-info">
-                    <h2>${bikeName}</h2>
-                    <p>${translations[lang].rentalText}: ${bike.rental}</p>
-                    <p>${bikeComment}</p>
+                    <h2 class="bike-name">${bikeName}</h2>
+                    <p class="bike-rental">${translations[lang].rentalText}: <span class="rental-price">${bike.rental}</span></p> <!-- Цена аренды -->
+                    <p class="bike-comment">${bikeComment}</p>
+                </div>
+                
+                <div class="control">
                     <button class="apply-button">${applyButton}</button>
                     <select class="city-dropdown">
                         ${citiesList.map(city => `<option value="${city.id}">${city.city}</option>`).join('')}
@@ -132,15 +160,37 @@ function displayBikes() {
     cityDropdowns.forEach(dropdown => {
         dropdown.addEventListener('change', function() {
             const bikeId = this.closest('.bike-announcement').getAttribute('data-bike-id');
-            application.city = this.value;
+            const cityId = this.closest('.bike-announcement').querySelector('.city-dropdown').value;
+
+            application.city = cityId;
             application.bikeId = bikeId;
 
-            console.log('City changed to: ' + this.value);
+            console.log('City changed to: ' + cityId);
             console.log('Bike changed to: ' + bikeId);
 
             displayApplication();
         });
     });
+
+
+    const applyButtons = bikeListBlock.querySelectorAll('.apply-button');
+    applyButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const bikeId = this.closest('.bike-announcement').getAttribute('data-bike-id');
+            const cityId = this.closest('.bike-announcement').querySelector('.city-dropdown').value;
+
+            application.city = cityId;
+            application.bikeId = bikeId;
+
+            console.log('City changed to: ' + cityId);
+            console.log('Bike changed to: ' + bikeId);
+
+            displayApplication();
+            scrollToApplicationBlock();
+        });
+    });
+
+
 
     displayApplication();
 }
@@ -173,12 +223,214 @@ function displayNoBikes() {
             ${translations[lang].noBikesMessage}
         </div>
     `;
+
+    sendApplicationBlock.innerHTML = ``;
 }
 
 function displayApplication() {
-    sendApplicationBlock.classList.remove('hidden');
-
     sendApplicationBlock.innerHTML = `
+        <form id="applicationForm">
+            <div class="form-group">
+                <label for="name">${translations[lang].nameLabel}</label>
+                <input type="text" id="name" name="name" placeholder="${translations[lang].namePlaceholder}" required>
+                <span class="error-message" style="display:none;">${translations[lang].invalidName}</span>
+            </div>
+            <div class="form-group">
+                <label for="phone">${translations[lang].phoneLabel}</label>
+                <input type="tel" id="phone" name="phone" pattern="^[1-9][0-9]{8}$" placeholder="${translations[lang].phonePlaceholder}" required>
+                <span class="error-message" style="display:none;">${translations[lang].invalidPhone}</span>
+            </div>
+            <div class="form-group">
+                <label for="email">${translations[lang].emailLabel}</label>
+                <input type="email" id="email" name="email" placeholder="${translations[lang].emailPlaceholder}" required>
+                <span class="error-message" style="display:none;">${translations[lang].invalidEmail}</span>
+            </div>
+            <div class="form-group">
+                <label for="city">${translations[lang].cityLabel}</label>
+                <select id="city" name="city" required>
+                    ${citiesList.map(city => `<option value="${city.id}" ${city.id === Number(application.city) ? 'selected' : ''}>${city.city}</option>`).join('')}
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="bike">${translations[lang].bicycleLabel}</label>
+                <select id="bike" name="bike" required>
+                    ${bikesList.map(bike => `<option value="${bike.id}" ${bike.id === Number(application.bikeId) ? 'selected' : ''}>${bike['name' + lang.charAt(0).toUpperCase() + lang.slice(1)]}</option>`).join('')}
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="comment">${translations[lang].commentLabel}</label>
+                <textarea id="comment" name="comment" placeholder="${translations[lang].commentPlaceholder}"></textarea>
+                <span class="error-message" style="display:none;">${translations[lang].invalidComment}</span>
+            </div>
+            <input type="hidden" id="bikeId" name="bikeId" value="${application.bikeId}">
+            <input type="hidden" id="timestamp" name="timestamp">
+            <button type="submit">${translations[lang].applyButton}</button>
+        </form>
         
+        <div id="alert-success" class="alert alert-success" style="display:none;">
+          ${translations[lang].applicationSuccess}
+        </div>
+        <div id="alert-danger" class="alert alert-danger" style="display:none;">
+          ${translations[lang].applicationError}
+        </div>
     `;
+
+    document.getElementById('phone').addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/[^0-9]/g, '');
+
+        if (e.target.value.length > 9) {
+            e.target.value = e.target.value.slice(0, 9);
+        }
+
+        if (e.target.value.startsWith('0')) {
+            e.target.value = '';
+        }
+    });
+
+    document.getElementById('name').addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/[^a-zA-Zа-яА-ЯёЁ\s]/g, '');
+    });
+
+    document.getElementById('applicationForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        submitApplication();
+    });
+
+    document.getElementById('email').addEventListener('input', function() {
+        const isValid = validateEmail(this.value);
+        emailOk = isValid;
+        displayValidationMessage(this, isValid);
+    });
+
+    document.getElementById('phone').addEventListener('input', function() {
+        const isValid = validatePhone(this.value);
+        phoneOk = isValid;
+        displayValidationMessage(this, isValid);
+    });
+
+    document.getElementById('name').addEventListener('input', function() {
+        const isValid = validateName(this.value);
+        nameOk = isValid;
+        displayValidationMessage(this, isValid);
+    });
+
+    document.getElementById('comment').addEventListener('input', function() {
+        const isValid = validateComment(this.value);
+        commentOk = isValid;
+        displayValidationMessage(this, isValid);
+    });
+}
+
+function displayAlert(message, type) {
+    const alertSuccess = document.getElementById('alert-success');
+    const alertDanger = document.getElementById('alert-danger');
+    if (type === 'success') {
+        alertSuccess.textContent = translations[lang].applicationSuccess;
+        alertSuccess.style.display = 'block';
+        alertDanger.style.display = 'none';
+    } else if (type === 'danger') {
+        alertDanger.textContent = translations[lang].applicationError + message;
+        alertDanger.style.display = 'block';
+        alertSuccess.style.display = 'none';
+    }
+
+    if (type === 'success') {
+        alertSuccess.style.animation = 'slideUp 0.5s ease-out';
+    } else {
+        alertDanger.style.animation = 'slideUp 0.5s ease-out';
+    }
+
+    setTimeout(() => {
+        if (type === 'success') {
+            alertSuccess.style.animation = 'slideDown 0.5s ease-out';
+        } else {
+            alertDanger.style.animation = 'slideDown 0.5s ease-out';
+        }
+        setTimeout(() => {
+            alertSuccess.style.display = 'none';
+            alertDanger.style.display = 'none';
+        }, 500);
+
+        if (type === 'success') {
+            sendApplicationBlock.style.animation = 'slideDownApplication 0.5s ease-out';
+            scrollToBikesListBlock();
+
+            setTimeout(() => {
+                sendApplicationBlock.innerHTML = ``;
+            }, 500);
+        }
+    }, 3000);
+}
+
+function submitApplication() {
+    if(nameOk && emailOk && phoneOk && commentOk) {
+        const applicationData = {
+            name: document.getElementById('name').value,
+            phone: document.getElementById('phone').value,
+            email: document.getElementById('email').value,
+            city: document.getElementById('city').value,
+            comment: document.getElementById('comment').value,
+            bikeId: document.getElementById('bikeId').value,
+            timestamp: new Date().getTime()
+        };
+
+        fetch('http://localhost:8080/send_application', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(applicationData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.text(); // Используйте text вместо json
+            })
+            .then(data => {
+                console.log('Application send: ', data);
+                displayAlert('', 'success');
+            })
+            .catch(error => {
+                console.error('Error: ', error);
+                displayAlert(error.message, 'danger');
+            });
+    } else {
+        displayAlert("Check the entered data.", 'danger');
+
+        document.querySelectorAll('.error-message').forEach(el => {
+            if (el.style.display !== 'none') {
+                el.classList.add('error-shake');
+
+                el.addEventListener('animationend', () => {
+                    el.classList.remove('error-shake');
+                }, {once: true});
+            }
+        });
+    }
+}
+
+function validateEmail(email) {
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
+}
+
+function validatePhone(phone) {
+    return /^[1-9][0-9]{8}$/.test(phone);
+}
+
+function validateName(name) {
+    return /^[A-Za-zА-Яа-яЁё\s]+$/.test(name);
+}
+
+function validateComment(comment) {
+    return comment !== null && comment.trim().length > 0;
+}
+
+function displayValidationMessage(inputElement, isValid) {
+    const messageElement = inputElement.nextElementSibling;
+    if (!isValid) {
+        messageElement.style.display = 'block';
+    } else {
+        messageElement.style.display = 'none';
+    }
 }
